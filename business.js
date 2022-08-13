47b2b186a8b099f5905024fa01257b7a2902b9a0
guvin-vintage <109299594+guvin-vintage@users.noreply.github.com>
2022-08-13 17:24:55 +0700

    a

--- business.js
+++ business.js
@@ -74,7 +74,17 @@ $(document).ready(function(){
         },1000)
     })
 
-   
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animate__animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
+
 })
 
 function init()
